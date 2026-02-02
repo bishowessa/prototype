eb335d9516d90bf4477e90403d35878c52792909
0marAmara <onan383@outlook.com>
2026-02-03 01:14:06 +0200

    refactor: Update import paths to use @app alias and enhance routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('@app/pages/landing-page/landing-page').then((m) => m.LandingPage),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('@app/pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('@app/pages/signup/signup.component').then((m) => m.SignUpComponent),
+  },
+  { path: '**', redirectTo: '' },
+];
